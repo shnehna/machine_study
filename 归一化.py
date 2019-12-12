8b90d91b51b2d73f7e34fd85644798e19a18615b
shnehna <mr_shenhan@outlook.com>
2019-12-12 23:12:12 +0800

    归一化

---
+++ 归一化.py
@@ -0,0 +1,16 @@
+from sklearn.preprocessing import MinMaxScaler
+
+
+def mn():
+    """
+    归一化处理
+    :return: None
+    """
+    mn = MinMaxScaler(feature_range=(0, 2))
+    data = mn.fit_transform([[90, 2, 10, 40], [60, 4, 15, 45], [75, 3, 13, 46]])
+    print(data)
+    return None
+
+
+if __name__ == '__main__':
+    mn()
